58403bd531a7954c4eec65bbef4a4e36dbe1cc93
unknown <khieu.duongvan@gmail.com>
2022-11-24 16:40:30 +0900

    edit2 HomeScreen.js file

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -45,7 +45,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>Amazona new</title>
       </Helmet>
       <h1>Featured Products</h1>
      
