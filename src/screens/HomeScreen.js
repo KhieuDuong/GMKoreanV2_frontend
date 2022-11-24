caa38132c1bf45485d7229f880adaffe07d693bb
unknown <khieu.duongvan@gmail.com>
2022-11-24 16:25:50 +0900

    edit HomeScreen.js file

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -48,25 +48,7 @@ function HomeScreen() {
         <title>Amazona</title>
       </Helmet>
       <h1>Featured Products</h1>
-      <div className="products">
-        {loading ? (
-          <LoadingBox />
-        ) : error ? (
-          <MessageBox variant="danger">{error}</MessageBox>
-        ) : (
-         // <Row>
-    
-          //  {products.map((product) => (
-           //   <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
-           //     <Product product={product}></Product>
-          //    </Col>
-          //  ))}
-          //</Row>
-          <Row>
-            <p>Hello world</p>
-          </Row>
-        )}
-      </div>
+     
     </div>
   );
 }
